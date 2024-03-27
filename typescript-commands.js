6b93e120d0dcb167f48874eadbc625d0516ed7ef
Lev Odintsov <leo-shturm777@mail.ru>
2024-03-27 09:28:35 +0300

    ts commands

---
+++ typescript-commands.js
@@ -0,0 +1,6 @@
+{
+    // typescript основные команды
+    // npm install -g typescript - установить тайпскрипт глобально
+    // tsc main.ts - скомпилирвоать файл
+    // tsc --init tsconfig.json
+}
