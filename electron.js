44117fd1b0faf76a6ec27a6d10c002b0c5dd6c51
Lev Odintsov <leo-shturm777@mail.ru>
2024-03-27 09:27:11 +0300

    electron.js

---
+++ electron.js
@@ -0,0 +1,10 @@
+{
+    // шпора по electron js
+    // npm i -g @electron-forge/cli - установить электрон глобально
+    // electron-forge init electron app - инициализировать проект электрон
+    // npm run start
+    // npm run build
+    // serve -s build
+    // mv ./build/* ../electron/src/
+    // npm run make - сборка в установочный и исполняемый файлы
+}
